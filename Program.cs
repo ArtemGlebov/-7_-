6aaff332999444ead37042d1ac84d08e8cfd6474
Artem Glebov <artemglebov1985@gmail.com>
2019-06-10 23:18:30 +0300

    01

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_7_Фабрика_Фигур
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square aquare = new Square();
+            aquare.display();
+            aquare.perimeter();
+            aquare.area();
+            aquare.colorRandom();
+            aquare.getInfo();
+            //Console.WriteLine("Выбирите фигуру: ");
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_7_Фабрика_Фигур
+{
+    public class Square
+    {
+        private int square;
+        int side;                        //сторона
+        int perimeterP;                 //периметр
+        int areaS;                      //площадь
+        string [] color = { "чёрный", "белый", "зелёный", "синий", "красный", "жёлтый", "оранжевый", "глубой", "серый", "коричневый" }; //цвет
+        string colorRan;
+        
+        
+
+        public void display()
+        {
+            Console.WriteLine("Введите длину стороны: ");
+            side = int.Parse(Console.ReadLine()); // полученная сторона
+        }
+        public void perimeter() //периметр квадрата
+        {
+            perimeterP = 4 * side;
+        }
+        public void area()    // площадь квадрата
+        {
+            areaS = side * side;
+        }
+        public void colorRandom () //ранндомный цвет квадрата
+        {
+            Random ran = new Random();
+            int nums = (ran.Next(0, 9));
+            colorRan = Convert.ToString(color[nums]);
+        }
+
+        
+          public void getInfo()
+        {
+            Console.WriteLine($" Параметры квадрата: \n Периметр: {perimeterP}\n Площадь: {areaS}\n Цвет: {colorRan}");
+        }
+    }
+}
