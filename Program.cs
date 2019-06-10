07fe6e5e290bb2e86431907af849c7be9b19f661
Artem Glebov <artemglebov1985@gmail.com>
2019-06-11 01:10:35 +0300

    03_ нужно сделать бесконечный цикл

--- Program.cs
+++ Program.cs
@@ -6,31 +6,52 @@ namespace Task_7_Фабрика_Фигур
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Выберите фигуру 1-квадрат 2-круг 3-треугольник");
+            
+            Console.WriteLine("Выберите фигуру 1-квадрат, 2-круг, 3-треугольник, 0-выйти из программы");
+            int figure = int.Parse(Console.ReadLine()); //выбор фигуры
 
-            //квадрат
-            Square aquare = new Square();
-            aquare.display();
-            aquare.perimeter();
-            aquare.area();
-            aquare.colorRandom();
-            aquare.getInfo();
+             switch (figure)
+           {
+               
+                case 1: //квадрат
+                Console.WriteLine("Вы выбрали квадра: ");
+                Square aquare = new Square();
+                aquare.display();
+                aquare.perimeter();
+                aquare.area();
+                aquare.colorRandom();
+                aquare.getInfo();
+                    break;
 
-            //круг
-            Сircle circle = new Сircle();
-            circle.display();
-            circle.perimeter();
-            circle.area();
-            circle.colorRandom();
-            circle.getInfo();
+                case 2: //круг
+                    Console.WriteLine("Вы выбрали круг: ");
+                    Сircle circle = new Сircle();
+                    circle.display();
+                    circle.perimeter();
+                    circle.area();
+                    circle.colorRandom();
+                    circle.getInfo();
+                    break;
 
-            //треугольник
-            Triangle triangle = new Triangle();
-            triangle.display();
-            triangle.perimeter();
-            triangle.area();
-            triangle.colorRandom();
-            triangle.getInfo();
+                case 3: //треугольник
+                    Console.WriteLine("Вы выбрали треугольник: ");
+                    Triangle triangle = new Triangle();
+                    triangle.display();
+                    triangle.perimeter();
+                    triangle.area();
+                    triangle.colorRandom();
+                    triangle.getInfo();
+                    break;
+
+                case 0:
+                    Console.WriteLine("Вы вышли из программы!");
+                    break;
+
+                default:
+                    Console.WriteLine("Не верно введи номер фигуры");
+                   break;
+
+            }
         }
     }
 }
